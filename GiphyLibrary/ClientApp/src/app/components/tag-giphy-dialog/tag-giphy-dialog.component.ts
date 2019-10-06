91e9dc7c1a64a974ff22c97e36d20dbe285290d6
bnikaido <breenik14@gmail.com>
2019-10-06 16:42:52 -0500

    Added tag input

--- GiphyLibrary/ClientApp/src/app/components/tag-giphy-dialog/tag-giphy-dialog.component.ts
+++ GiphyLibrary/ClientApp/src/app/components/tag-giphy-dialog/tag-giphy-dialog.component.ts
@@ -4,15 +4,21 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 @Component({
   selector: 'app-tag-giphy-dialog',
   templateUrl: './tag-giphy-dialog.component.html',
+  styleUrls: ["./tag-giphy-dialog.component.css"]
 })
 export class TagGiphyDialogComponent {
   selectedTag: string;
+  inputTag: string;
 
   constructor(
     public dialogRef: MatDialogRef<TagGiphyDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public tags: string[]) { }
 
-  onBackClick(): void {
-    this.dialogRef.close();
+  onOkClick(): void {
+    var result = this.selectedTag;
+    if (result == "other") {
+      result = this.inputTag;
+    }
+    this.dialogRef.close(result);
   }
 }
